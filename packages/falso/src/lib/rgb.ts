7ff50045cdb2e39f082bf319748f36502baada40
Rai Siqueira <rai93siqueira@gmail.com>
2022-01-10 03:22:18 -0300

    feat: rgb function now support alpha

--- packages/falso/src/lib/rgb.ts
+++ packages/falso/src/lib/rgb.ts
@@ -2,6 +2,19 @@ function randomValue() {
   return Math.floor(Math.random() * (255 + 1));
 }
 
-export function rgb() {
-  return `rgb(${randomValue()}, ${randomValue()}, ${randomValue()})`;
+function randomFloat(min: number, max: number) {
+  return Number((Math.random() * (max - min) + min).toFixed(2));
+}
+
+export function rgb(alpha = false) {
+  const [r, g, b, a] = [
+    randomValue(),
+    randomValue(),
+    randomValue(),
+    randomFloat(0.1, 1),
+  ];
+  if (alpha) {
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
+  return `rgb(${r}, ${g}, ${b})`;
 }
