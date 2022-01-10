2bb6483404c4a794e7b89b2353d9a4f5436e92cd
Rai Siqueira <rai93siqueira@gmail.com>
2022-01-10 03:22:18 -0300

    feat: rgb function now support alpha

--- packages/falso/src/tests/rgb.spec.ts
+++ packages/falso/src/tests/rgb.spec.ts
@@ -1,10 +1,17 @@
-import { rgb } from "../lib/rgb";
+import { rgb } from '../lib/rgb';
 
 describe('rgb', () => {
   it('should return a rgb string in the correct format', () => {
     const expected = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
 
     expect(rgb()).toEqual(expect.stringMatching(expected));
+  });
+
+  it('should return a rgba string in the correct format', () => {
+    const expected =
+      /rgba\((\d{1,3}), (\d{1,3}), (\d{1,3}), \s*(\d*(?:\.\d+)?)\)$/;
+
+    expect(rgb(true)).toEqual(expect.stringMatching(expected));
   });
 
   it('should return red value between 0 and 255', () => {
@@ -21,10 +28,12 @@ describe('rgb', () => {
     expect(colourValues?.[1]).toBeLessThanOrEqual(255);
   });
 
-  it('should return blue value between 0 and 255', () => {
-    const colourValues = rgb().match(/\d+/g)?.map(Number);
+  it('should return alpha value between 0.1 and 1', () => {
+    const colourValues = rgb(true)
+      .match(/\d*(?:\.\d+)/)
+      ?.map(Number);
 
-    expect(colourValues?.[2]).toBeGreaterThanOrEqual(0);
-    expect(colourValues?.[2]).toBeLessThanOrEqual(255);
+    expect(colourValues?.[0]).toBeGreaterThanOrEqual(0.1);
+    expect(colourValues?.[0]).toBeLessThanOrEqual(1);
   });
 });
