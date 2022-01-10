1e7131a35a4cc2686683a82eaf0e8b9e3406d5fd
Rai Siqueira <rai93siqueira@gmail.com>
2022-01-10 10:53:28 -0300

    feat: add function to generate random float

--- packages/falso/src/lib/random-float.ts
+++ packages/falso/src/lib/random-float.ts
@@ -1,11 +1,16 @@
 type Options = {
-  min: number;
-  max: number;
+  min?: number;
+  max?: number;
   fractions?: number;
 };
 
-export function randomFloat(
-  { min, max, fractions }: Options = { min: 1.0, max: 999.99 }
-) {
-  return Number((Math.random() * (max - min) + min).toFixed(fractions ?? 2));
+export function randomFloat({ min, max, fractions }: Options = {}) {
+  const [minValue, maxValue, fractionValue] = [
+    min ?? 1.0,
+    max ?? 9999.99,
+    fractions ?? 2,
+  ];
+  return Number(
+    (Math.random() * (maxValue - minValue) + minValue).toFixed(fractionValue)
+  );
 }
--- packages/falso/src/tests/random-float.spec.ts
+++ packages/falso/src/tests/random-float.spec.ts
@@ -2,7 +2,7 @@ import { randomFloat } from '../lib/random-float';
 
 describe('random-float', () => {
   it('should return a random float between min and max default values', () => {
-    const [min, max] = [1.0, 999.99];
+    const [min, max] = [1.0, 9999.99];
     expect(randomFloat()).toBeGreaterThanOrEqual(min);
     expect(randomFloat()).toBeLessThanOrEqual(max);
   });
@@ -11,5 +11,17 @@ describe('random-float', () => {
     const [min, max] = [1.1, 20.0];
     expect(randomFloat({ min, max })).toBeGreaterThanOrEqual(min);
     expect(randomFloat({ min, max })).toBeLessThanOrEqual(max);
+  });
+
+  it('should return a random float when receive only min value', () => {
+    const min = 1.1;
+    expect(randomFloat({ min })).toBeGreaterThanOrEqual(min);
+    expect(randomFloat({ min })).toBeLessThanOrEqual(9999.99);
+  });
+
+  it('should return a random float when receive only max value', () => {
+    const max = 20.0;
+    expect(randomFloat({ max })).toBeLessThanOrEqual(max);
+    expect(randomFloat({ max })).toBeGreaterThanOrEqual(1.0);
   });
 });
