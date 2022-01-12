24996614a9ea9f8c36fa33a7cbc49f32a622ab3e
Ryan <theryansmee@gmail.com>
2022-01-12 19:19:14 +0000

    refactor: 💡 Minimise sentence building boilerplate
    Replace sentence structure boiletplate with dynamic sentence builder
    logic
    
    ✅ Closes: #15

--- packages/falso/src/lib/text.ts
+++ packages/falso/src/lib/text.ts
@@ -1,37 +1,54 @@
-import { rand } from './core';
 import { word } from './word';
 
-function randomSentence() {
-  return rand([
-    `${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()}, ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()}, ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()}, ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} - ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()} ${word()} ${word()} ${word()} ${word()} (${word()} ${word()} ${word()} ${word()})`,
-    `${word()} ${word()} "${word()} ${word()}" ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-    `${word()} ${word()}, ${word()} ${word()} ${word()} ${word()} ${word()} ${word()} ${word()}, ${word()} ${word()} ${word()} ${word()} ${word()}.`,
-  ]);
-}
+function getSpecialCharacter(): string {
+  const randomNumber = Math.floor(Math.random() * 10);
 
-export function text({ charCount } = { charCount: 10 }): string | void {
-  if (charCount < 1) {
-    throw 'Character count must be greater than 0';
-    return;
+  if (randomNumber === 1) {
+    return '.';
   }
 
-  let text = randomSentence();
+  if (randomNumber === 2) {
+    return ',';
+  }
+
+  return '';
+}
+
+function generateSentence(charCount: number): string {
+  let text = word({ capitalise: true });
+  let capitalise = false;
+  let totalWords = 1;
 
   while (text.length < charCount) {
-    text = `${text} ${randomSentence()}`;
+    let randomWord: string;
+    let specialChar = '';
+
+    if (capitalise) {
+      randomWord = word({ capitalise: true });
+      capitalise = false;
+    } else {
+      randomWord = word();
+    }
+
+    if (totalWords > 2) {
+      specialChar = getSpecialCharacter();
+    }
+
+    if (specialChar === '.') {
+      capitalise = true;
+    }
+
+    text += ` ${randomWord}${specialChar}`;
+    totalWords++;
   }
 
   return text.substring(0, charCount);
+}
+
+export function text({ charCount } = { charCount: 10 }): string | void {
+  if (charCount < 1 || !charCount) {
+    throw 'Character count must be greater than 0';
+  }
+
+  return generateSentence(charCount);
 }
--- packages/falso/src/tests/text.spec.ts
+++ packages/falso/src/tests/text.spec.ts
@@ -10,7 +10,15 @@ describe('text', () => {
       });
     });
 
-    describe('charCount of 1', () => {
+    describe('charCount is NaN', () => {
+      it('should thrown min value error', () => {
+        const result = () => text({ charCount: NaN });
+
+        expect(result).toThrow('Character count must be greater than 0');
+      });
+    });
+
+    describe('charCount is 1', () => {
       it('should return a string with 1 characters', () => {
         const result = text({ charCount: 1 });
 
@@ -18,7 +26,7 @@ describe('text', () => {
       });
     });
 
-    describe('charCount of 100', () => {
+    describe('charCount is 100', () => {
       it('should return a string with 100 characters', () => {
         const result = text({ charCount: 100 });
 
@@ -26,7 +34,7 @@ describe('text', () => {
       });
     });
 
-    describe('charCount of 5000', () => {
+    describe('charCount is 5000', () => {
       it('should return a string with 5000 characters', () => {
         const result = text({ charCount: 5000 });
 
