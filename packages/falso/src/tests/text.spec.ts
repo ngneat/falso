0f164789db533a5a0bc7ebd008887aae14f0645c
Ryan <theryansmee@gmail.com>
2022-01-12 08:13:57 +0000

    test: 🥳 Fix tests so they fail if error is not thrown
    Use toThrow instead of try catch block to ensure that tests fail when
    error is not thrown
    
    ✅ Closes: #15

--- packages/falso/src/tests/text.spec.ts
+++ packages/falso/src/tests/text.spec.ts
@@ -1,20 +1,17 @@
-import { text } from "../lib/text";
+import { text } from '../lib/text';
 
 describe('text', () => {
   describe('charCount IS passed', () => {
     describe('charCount of 0', () => {
-      it('should trigger console error', () => {
-        try {
-          text({ charCount: 0 });
-        } catch (error) {
-          expect(error).toEqual('Character count must be greater than 0');
-        }
+      it('should thrown min value error', () => {
+        const result = () => text({ charCount: 0 });
+
+        expect(result).toThrow('Character count must be greater than 0');
       });
     });
 
     describe('charCount of 1', () => {
       it('should return a string with 1 characters', () => {
-
         const result = text({ charCount: 1 });
 
         expect(result?.length).toEqual(1);
@@ -23,7 +20,6 @@ describe('text', () => {
 
     describe('charCount of 100', () => {
       it('should return a string with 100 characters', () => {
-
         const result = text({ charCount: 100 });
 
         expect(result?.length).toEqual(100);
@@ -32,7 +28,6 @@ describe('text', () => {
 
     describe('charCount of 5000', () => {
       it('should return a string with 5000 characters', () => {
-
         const result = text({ charCount: 5000 });
 
         expect(result?.length).toEqual(5000);
@@ -40,19 +35,16 @@ describe('text', () => {
     });
 
     describe('charCount of 5001', () => {
-      it('should trigger console error', () => {
-        try {
-          text({ charCount: 5001 });
-        } catch (error) {
-          expect(error).toEqual('Character count must be less than 5000');
-        }
+      it('should throw max value error', () => {
+        const result = () => text({ charCount: 5001 });
+
+        expect(result).toThrow('Character count must be less than 5000');
       });
     });
   });
 
   describe('charCount is NOT passed', () => {
     it('should return a string with 10 characters', () => {
-
       const result = text();
 
       expect(result?.length).toEqual(10);
