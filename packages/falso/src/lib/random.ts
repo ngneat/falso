77ccaed2b9a14d6f9db758018344e318ef106445
Shahar Kazaz <shahar.kazaz@gmail.com>
2022-01-17 01:43:07 +0200

    fix: 🐞 export seed function
    Export the seed function, fix random function docs
    
    ✅ Closes: #46

--- packages/falso/src/lib/random.ts
+++ packages/falso/src/lib/random.ts
@@ -9,13 +9,26 @@ let prng = seedRandom();
  *
  * @example
  *
- * randRandom()
+ * random()
  *
  */
 export function random() {
   return prng();
 }
 
+/**
+ * Sets and Resets random seed.
+ *
+ * @category general
+ *
+ * @example
+ *
+ * seed()
+ *
+ * @example
+ *
+ * seed('some-constant-seed')
+ */
 export function seed(value?: string) {
   prng = seedRandom(value);
 }
