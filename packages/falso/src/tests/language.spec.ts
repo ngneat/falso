a36145aecb1a885018f9ee056090152e16159eee
Michael Völker <mver@posteo.de>
2022-01-18 20:11:54 +0100

    test: 🥳 add missing test for language codes
    ✅ Closes: #51

--- packages/falso/src/tests/language.spec.ts
+++ packages/falso/src/tests/language.spec.ts
@@ -4,4 +4,7 @@ describe('language', () => {
   it('should create', () => {
     expect(randLanguage).toBeTruthy();
   });
+  it('should create ISO 639-1 compliant language codes', () => {
+    expect(randLanguage({ code: true })).toHaveLength(2);
+  });
 });
