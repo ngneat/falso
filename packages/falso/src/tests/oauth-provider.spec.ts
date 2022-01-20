13644d3bc4f398963dd8eb3cb49e210157c3f38c
Ryan <theryansmee@gmail.com>
2022-01-20 16:24:36 +0000

    test: 🥳 Remove unnecessary spec file
    ✅ Closes: #72

--- packages/falso/src/tests/oauth-provider.spec.ts
+++
@@ -1,7 +0,0 @@
-import { randOAuthProvider } from '../lib/oauth-provider';
-
-describe('oauthProvider', () => {
-  it('should create', () => {
-    expect(randOAuthProvider).toBeTruthy();
-  });
-});
