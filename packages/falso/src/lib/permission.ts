40dd647fcd7292e011ed4fcf7e58c0c373837daa
Michael Völker <michael.voelker@studysmarter.de>
2022-01-20 20:05:57 +0100

    refactor: 💡 make randPermission simpler

--- packages/falso/src/lib/permission.ts
+++ packages/falso/src/lib/permission.ts
@@ -3,7 +3,6 @@ import { data } from './permission.json';
 
 interface PermissionOptions extends FakeOptions {
   numeric?: boolean;
-  combination?: boolean;
 }
 
 /**
@@ -18,27 +17,17 @@ interface PermissionOptions extends FakeOptions {
  * @example
  *
  * randPermission({ length: 10 })
+ *
+ * @example
+ *
  * randPermission({ numeric: true })
- * randPermission({ combination: true })
- * randPermission({ numeric: true, combination: true })
  *
  */
 export function randPermission<Options extends PermissionOptions>(
   options?: Options
 ) {
-  if (options?.numeric && options?.combination) {
-    return fake([0, 1, 2, 3, 4, 5, 6, 7], options);
-  }
-
   if (options?.numeric) {
     return fake([0, 1, 2, 4], options);
-  }
-
-  if (options?.combination) {
-    return fake(
-      ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'rw-', 'rwx'],
-      options
-    );
   }
 
   return fake(data, options);
