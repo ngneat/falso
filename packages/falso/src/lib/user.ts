9cb16bf7a5aad9504d127c804897f75bd2d04ab5
arulprabakaran <arul.prabakaran@gmail.com>
2022-01-22 00:59:30 +0530

    feat: added phone number formats

--- packages/falso/src/lib/user.ts
+++ packages/falso/src/lib/user.ts
@@ -46,7 +46,7 @@ export function randUser<Options extends FakeOptions>(options?: Options) {
       email: randEmail(),
       firstName: randFirstName(),
       lastName: randLastName(),
-      phone: randPhoneNumber(),
+      phone: <string>randPhoneNumber(),
       img: randAvatar(),
       username: randUserName(),
       address: {
---
+++ packages/falso/src/tests/phone-number.spec.ts
@@ -0,0 +1,23 @@
+import { randPhoneNumber } from '../lib/phone-number';
+
+describe('randPhoneNumber', () => {
+  it('should return a phone number', () => {
+    expect(randPhoneNumber()).toMatch(/^[\d\s+()[\]]+$/);
+  });
+
+  it('should return a UK number', () => {
+    expect(
+      randPhoneNumber({
+        countryCode: 'IN',
+      })
+    ).toMatch(/^[\d\s+()[\]]+$/);
+  });
+
+  it('should return 10 phone number', () => {
+    const phoneNumbers = randPhoneNumber({
+      length: 10,
+      countryCode: 'IN',
+    });
+    expect(phoneNumbers?.length).toEqual(10);
+  });
+});
