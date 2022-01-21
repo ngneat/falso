fefcc4abaae81b78b5f1fc792c125e1586f752bc
Michael Völker <michael.voelker@studysmarter.de>
2022-01-21 06:53:04 +0100

    test: 🥳 test correct return value type

--- packages/falso/src/tests/clothing-size.spec.ts
+++ packages/falso/src/tests/clothing-size.spec.ts
@@ -1,10 +1,7 @@
 import { randClothingSize } from '../lib/clothing-size';
 
 describe('clothingSize', () => {
-  it('should create', () => {
-    expect(randClothingSize).toBeTruthy();
-  });
   it('should create one clothing size', () => {
-    expect(randClothingSize()).toBeTruthy();
+    expect(typeof randClothingSize()).toBe('string');
   });
 });
