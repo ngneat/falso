38cd2581a9478d6472353da0550a1715a3215ea2
arulprabakaran <arul.prabakaran@gmail.com>
2022-01-22 03:10:30 +0530

    feat: updated to accept user specified mask

--- packages/falso/src/lib/mask.ts
+++ packages/falso/src/lib/mask.ts
@@ -11,12 +11,44 @@ import { fake, FakeOptions, getRandomInRange } from './core/core';
  *
  * @example
  *
+ * randMask({ mask: "@#### @##" })
+ *
+ * @example
+ *
  * randMask({ length: 10 })
  *
  */
-export function randMask<Options extends FakeOptions>(options?: Options) {
-  return fake(
-    () => getRandomInRange({ min: 1000, max: 9999 }).toString(),
-    options
-  );
+export function randMask<
+  Options extends FakeOptions & {
+    mask?: string;
+    char?: string;
+    digit?: string;
+  }
+>(options?: Options) {
+  return fake(() => {
+    const [mask, char, digit] = [
+      options?.mask ?? '@##@#',
+      options?.char ?? '@',
+      options?.digit ?? '#',
+    ];
+
+    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const randomDigits = '0123456789';
+
+    return [...mask]
+      .map((item) => {
+        let replaceChar = '';
+        if (item == char)
+          replaceChar = randomChars.charAt(
+            Math.floor(Math.random() * randomChars.length)
+          );
+        else if (item == digit)
+          replaceChar = randomDigits.charAt(
+            Math.floor(Math.random() * randomDigits.length)
+          );
+        else replaceChar = item;
+        return replaceChar;
+      })
+      .join('');
+  }, options);
 }
