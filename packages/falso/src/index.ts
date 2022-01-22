aa71937fa1bdf92aeb5289564df71fd064df4184
yalhyane <y.alhyane@gmail.com>
2022-01-22 03:18:01 +0100

    fix: 🐞 export randPronoun and randQuote functions
    Export randPronoun and randQuote functions which fixes text docs page
        
        ✅ Closes: #99

--- packages/falso/src/index.ts
+++ packages/falso/src/index.ts
@@ -149,3 +149,5 @@ export { randBrand } from './lib/brand';
 export { randPermission } from './lib/permission';
 export { randEmailProvider } from './lib/email-provider';
 export { randClothingSize } from './lib/clothing-size';
+export { randPronoun } from './lib/pronoun';
+export { randQuote } from './lib/quote';
