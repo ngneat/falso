88543249affce89a7b1cf88529b19d8cdf34475c
ttyago <ttyago.dev@gmail.com>
2022-01-23 18:29:31 +0100

    refactor: 💡 randSocial TEST
    ✅ Closes: #113

--- packages/falso/src/tests/social.spec.ts
+++ packages/falso/src/tests/social.spec.ts
@@ -1,19 +1,18 @@
 import { randSocial } from '../lib/social';
 
 describe('Social', () => {
-  it('Shuould return social object in formats [{name, link}]', () => {
-    const social = randSocial();
+  it('Should return social object in formats [{name, link}]', () => {
+    const test = randSocial();
 
-    expect(social.name != '');
-    expect(social.link != '');
+    expect(test.name && test.link);
   });
 
   it('Should return 10 social object in formats [{name, link}]', () => {
     const social = randSocial({ length: 10 });
 
     expect(social.length).toEqual(10);
-
-    // Test a random element to check if it's empty
-    expect(social[3].name != '' && social[3].link != '');
+    for (const el of social) {
+      expect(el.name && el.link);
+    }
   });
 });
