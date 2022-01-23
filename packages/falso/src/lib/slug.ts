511e682cbad8dc6d39cb97df434496fc2cb7acc1
Michael Völker <michael.voelker@studysmarter.de>
2022-01-23 22:12:56 +0100

    feat: 🔥 generate random slug
    ✅ Closes: #118

---
+++ packages/falso/src/lib/slug.ts
@@ -0,0 +1,24 @@
+import { FakeOptions, fake, getRandomInRange } from './core/core';
+import { randWord } from './word';
+
+/**
+ * Generate a random slug.
+ *
+ * @category Internet
+ *
+ * @example
+ *
+ * randSlug()
+ *
+ * @example
+ *
+ * randSlug({ length: 10 })
+ *
+ */
+export function randSlug<Options extends FakeOptions>(options?: Options) {
+  return fake(() => {
+    const numberOfWordsInSlug = getRandomInRange({ min: 1, max: 10 });
+    const randomWords = randWord({ length: numberOfWordsInSlug });
+    return randomWords.join('-');
+  }, options);
+}
