6098ed3662cb37128f5e52a82a6ffe4511b4a2c9
jasien <jasien_elimy@hotmail.co.uk>
2022-02-05 19:37:18 +0000

    feat: 🔥 Add random code snippet

---
+++ packages/falso/src/tests/code-snippet.spec.ts
@@ -0,0 +1,15 @@
+import { CODE_SNIPPET_LANGUAGES, randCodeSnippet } from '../lib/code-snippet';
+
+describe('code-snippet', () => {
+  it('should create one code snippet', () => {
+    expect(typeof randCodeSnippet()).toBe('string');
+  });
+
+  it('should have a code snippet for all CodeSnippetLang literals', () => {
+    CODE_SNIPPET_LANGUAGES.forEach((CODE_SNIPPET_LANGUAGE) => {
+      expect(typeof randCodeSnippet({ lang: CODE_SNIPPET_LANGUAGE })).toBe(
+        'string'
+      );
+    });
+  });
+});
