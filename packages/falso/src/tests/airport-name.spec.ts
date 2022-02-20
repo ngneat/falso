0fb3a4c3a029739ebd07a8e6ce712ffc6a68a8c7
Ryan Smee <theryansmee@gmail.com>
2022-02-20 17:52:51 +0100

    test: 🥳 Add test for randAirportName
    ✅ Closes: #208

--- packages/falso/src/tests/airport-name.spec.ts
+++ packages/falso/src/tests/airport-name.spec.ts
@@ -1,7 +1,12 @@
-import { randAirportName } from '../lib/airport-name';
+import { data } from '../lib/airport.json';
+import { randAirportCode } from '@ngneat/falso';
 
 describe('airportName', () => {
-  it('should create', () => {
-    expect(randAirportName).toBeTruthy();
+  it('should return an airport name', () => {
+    const airportCodes = data.map(({ name }) => name);
+
+    const result = randAirportCode();
+
+    expect(airportCodes).toContain(result);
   });
 });
