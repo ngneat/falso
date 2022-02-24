dc537f23bfe4333e55a50275d9e6607507c4cc7f
Ryan Smee <ryan@sourcebreaker.com>
2022-02-24 12:27:43 +0000

    feat: 🔥 Tweak card entity properties
    Add dates and update name format
    
    ✅ Closes: #215

--- packages/falso/src/lib/credit-card.ts
+++ packages/falso/src/lib/credit-card.ts
@@ -1,9 +1,17 @@
-import { FakeOptions, fake } from './core/core';
+import { fake, FakeOptions } from './core/core';
 import { randCreditCardCVV } from './credit-card-cvv';
 import { randCreditCardIssuer } from './credit-card-issuer';
-import { randCreditCardNumber } from './credit-card-number';
+import { Issuer, randCreditCardNumber } from './credit-card-number';
 import { randAccount } from './account';
 import { randFullName } from './full-name';
+import { rand } from './rand';
+import { randPastDate } from './past-date';
+import { randFutureDate } from './future-date';
+import { randPersonTitle } from './person-title';
+
+export interface CreditCardOptions extends FakeOptions {
+  issuer?: Issuer;
+}
 
 export interface CreditCard {
   ccv: string;
@@ -11,7 +19,9 @@ export interface CreditCard {
   number: string;
   account: string;
   name: string;
-  type: string;
+  type: 'Credit' | 'Debit';
+  validFrom: string;
+  untilEnd: string;
 }
 
 /**
@@ -28,17 +38,36 @@ export interface CreditCard {
  * randCreditCard({ length: 10 })
  *
  */
-export function randCreditCard<Options extends FakeOptions = never>(
+export function randCreditCard<Options extends CreditCardOptions = never>(
   options?: Options
 ) {
   const factory: () => CreditCard = () => {
+    const title = randPersonTitle();
+    const fullName = randFullName({ withAccents: false });
+    const name = `${title} ${fullName}`;
+    const issuer = options?.issuer ?? randCreditCardIssuer();
+    const dateOptions: Intl.DateTimeFormatOptions = {
+      month: 'numeric',
+      year: 'numeric',
+    };
+    const validFrom = randPastDate({ years: 1 }).toLocaleDateString(
+      'en-GB',
+      dateOptions
+    );
+    const untilEnd = randFutureDate({ years: 2 }).toLocaleDateString(
+      'en-GB',
+      dateOptions
+    );
+
     return {
+      name,
+      issuer,
+      validFrom,
+      untilEnd,
       ccv: randCreditCardCVV(),
-      issuer: randCreditCardIssuer(),
-      number: randCreditCardNumber(),
+      number: randCreditCardNumber({ issuer }),
       account: randAccount(),
-      name: randFullName({ withAccents: false }),
-      type: 'credit',
+      type: rand(['Credit', 'Debit']),
     };
   };
 
--- packages/falso/src/tests/credit-card.spec.ts
+++ packages/falso/src/tests/credit-card.spec.ts
@@ -3,7 +3,7 @@ import { randCreditCard } from '../lib/credit-card';
 describe('creditCard', () => {
   it('should create', () => {
     // console.log(randCreditCard());
-    // console.log(randCreditCard());
+    console.log(randCreditCard());
 
     expect(1);
   });
