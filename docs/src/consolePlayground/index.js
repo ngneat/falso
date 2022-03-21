fb75106b914dd90cb72de2c12855804bbabce9d3
Ryan <theryansmee@gmail.com>
2022-03-21 12:45:14 +0000

    feat: 🔥 Allow users to access falso on doc site console
    Add falso.min to window._ so users can try it out from the docs site
    
    ✅ Closes: #225

---
+++ docs/src/consolePlayground/index.js
@@ -0,0 +1,10 @@
+import * as falso from '../theme/ReactLiveScope/falso.min';
+
+window.onload = function () {
+	const titleStyles = 'color:#8a76d9; font-size: 22px; font-weight:bold;';
+	const copyStyles = 'color:white; font-size: 14px';
+	const exampleStyles = 'color:#8a76d9; font-size: 14px;';
+	console.log('%cFalso\n\n' + '%cYou can use _ to access Falso functions here in the console. \n\n%c' + 'For example: _.randFullName()', titleStyles, copyStyles, exampleStyles)
+
+	window._ = falso;
+};
