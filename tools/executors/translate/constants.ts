5ffb4017179e00a3c477398f2d89976f49f3ec3c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-07 15:40:47 +0800

    Delete tools directory

--- tools/executors/translate/constants.ts
+++
@@ -1,3 +0,0 @@
-export const UNICODE_REGEXP = /\\u([\d\w]{4})/gi;
-export const COMMENT = `// THIS FILE IS AUTO-GENERATED THROUGH \`npm run translate\``;
-export const TYPE_SCRIPT_FILE_EXTENSION = '.ts';
