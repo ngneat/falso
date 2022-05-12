0825d8f6d6062559c9055bdd4910981877c22d30
Ryan Smee <theyransmee@gmail.com>
2022-05-12 20:17:53 +0100

    fix: 🐞 Generate uuid from seed
    ✅ Closes: #232

--- packages/falso/src/lib/uuid.ts
+++ packages/falso/src/lib/uuid.ts
@@ -1,5 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4, V4Options } from 'uuid';
 import { fake, FakeOptions } from './core/core';
+import { randNumber } from './number';
 
 /**
  * Generate a random uuid.
@@ -18,5 +19,9 @@ import { fake, FakeOptions } from './core/core';
 export function randUuid<Options extends FakeOptions = never>(
   options?: Options
 ) {
-  return fake(() => uuidv4(), options);
+  const v4options: V4Options = {
+    random: randNumber({ min: 0, max: 255, length: 16 }),
+  };
+
+  return fake(() => uuidv4(v4options), options);
 }
---
+++ packages/falso/src/tests/uuid.spec.ts
@@ -0,0 +1,16 @@
+import { randUuid, seed } from '@ngneat/falso';
+
+describe('uuid', () => {
+  afterAll(() => {
+    seed();
+  });
+
+  it('should generate uuid from seed', () => {
+    seed('🙃🔥😊');
+    expect(randUuid()).toEqual('02c11a83-1678-4638-9e55-39d543c31599');
+    seed('🚀🚀🚀');
+    expect(randUuid()).toEqual('b9b0b6f3-8e6d-4853-b562-e726ee32fec2');
+    seed('🙃🔥😊');
+    expect(randUuid()).toEqual('02c11a83-1678-4638-9e55-39d543c31599');
+  });
+});
