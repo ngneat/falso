08a1102121933e21298b9ea66e13e81dad74461f
czlowiek488 <34620109+czlowiek488@users.noreply.github.com>
2022-05-20 18:06:00 +0200

    fix

--- packages/falso/src/index.ts
+++ packages/falso/src/index.ts
@@ -193,201 +193,212 @@ import { randAirport } from './lib/airport';
 import { randAirportName } from './lib/airport-name';
 import { randAirportCode } from './lib/airport-code';
 import { randCreditCard } from './lib/credit-card';
-export const randAbbreviation = randAbbreviation;
-export const randAccessory = randAccessory;
-export const randAccount = randAccount;
-export const randAlpha = randAlpha;
-export const randAlphaNumeric = randAlphaNumeric;
-export const randAmericanFootballTeam = randAmericanFootballTeam;
-export const randAmount = randAmount;
-export const randAnimalType = randAnimalType;
-export const randArn = randArn;
-export const randAvatar = randAvatar;
-export const randAwsRegion = randAwsRegion;
-export const randAwsService = randAwsService;
-export const randAwsRequestId = randAwsRequestId;
-export const randBaseballTeam = randBaseballTeam;
-export const randBasketballTeam = randBasketballTeam;
-export const randBear = randBear;
-export const randBetweenDate = randBetweenDate;
-export const randBic = randBic;
-export const randBinary = randBinary;
-export const randBird = randBird;
-export const randBitcoinAddress = randBitcoinAddress;
-export const randBook = randBook;
-export const randBoolean = randBoolean;
-export const randBrand = randBrand;
-export const randBrowser = randBrowser;
-export const randCardinalDirection = randCardinalDirection;
-export const randCat = randCat;
-export const randCatchPhrase = randCatchPhrase;
-export const randCetacean = randCetacean;
-export const randCity = randCity;
-export const randClothingSize = randClothingSize;
-export const randColor = randColor;
-export const randCompanyName = randCompanyName;
-export const randCountry = randCountry;
-export const randCountryCode = randCountryCode;
-export const randCounty = randCounty;
-export const randCow = randCow;
-export const randCreditCardCVV = randCreditCardCVV;
-export const randCreditCardBrand = randCreditCardBrand;
-export const randCreditCardNumber = randCreditCardNumber;
-export const randCrocodilia = randCrocodilia;
-export const randCurrencyCode = randCurrencyCode;
-export const randCurrencyName = randCurrencyName;
-export const randCurrencySymbol = randCurrencySymbol;
-export const randDatabaseCollation = randDatabaseCollation;
-export const randDatabaseColumn = randDatabaseColumn;
-export const randDatabaseEngine = randDatabaseEngine;
-export const randDatabaseType = randDatabaseType;
-export const randDepartment = randDepartment;
-export const randDirection = randDirection;
-export const randDirectoryPath = randDirectoryPath;
-export const randDog = randDog;
-export const randDomainName = randDomainName;
-export const randDomainSuffix = randDomainSuffix;
-export const randDrinks = randDrinks;
-export const randEmail = randEmail;
-export const randEmailProvider = randEmailProvider;
-export const randEmoji = randEmoji;
-export const randEthereumAddress = randEthereumAddress;
-export const randFileExt = randFileExt;
-export const randFileName = randFileName;
-export const randFilePath = randFilePath;
-export const randFileType = randFileType;
-export const randFirstName = randFirstName;
-export const randFish = randFish;
-export const randFloat = randFloat;
-export const randFontFamily = randFontFamily;
-export const randFontSize = randFontSize;
-export const randFood = randFood;
-export const randFootballTeam = randFootballTeam;
-export const randFrequency = randFrequency;
-export const randFullName = randFullName;
-export const randFutureDate = randFutureDate;
-export const randGender = randGender;
-export const randGitBranch = randGitBranch;
-export const randGitCommitEntry = randGitCommitEntry;
-export const randGitCommitMessage = randGitCommitMessage;
-export const randGitCommitSha = randGitCommitSha;
-export const randGitShortSha = randGitShortSha;
-export const randHex = randHex;
-export const randHexaDecimal = randHexaDecimal;
-export const randHorse = randHorse;
-export const randHsl = randHsl;
-export const randHttpMethod = randHttpMethod;
-export const randIban = randIban;
-export const randIceHockeyTeam = randIceHockeyTeam;
-export const randImg = randImg;
-export const randIntegration = randIntegration;
-export const randIp = randIp;
-export const randIpv6 = randIpv6;
-export const randJobArea = randJobArea;
-export const randJobDescriptor = randJobDescriptor;
-export const randJobTitle = randJobTitle;
-export const randJobType = randJobType;
-export const randLanguage = randLanguage;
-export const randLastName = randLastName;
-export const randLatitude = randLatitude;
-export const randLine = randLine;
-export const randLines = randLines;
-export const randLion = randLion;
-export const randLocale = randLocale;
-export const randLongitude = randLongitude;
-export const randMac = randMac;
-export const randMask = randMask;
-export const randMimeType = randMimeType;
-export const randMonth = randMonth;
-export const randMovie = randMovie;
-export const randMovieCharacter = randMovieCharacter;
-export const randMusicGenre = randMusicGenre;
-export const randNearbyGPSCoordinate = randNearbyGPSCoordinate;
-export const randNumber = randNumber;
-export const randOAuthProvider = randOAuthProvider;
-export const randOctal = randOctal;
-export const randOrdinalDirection = randOrdinalDirection;
-export const randParagraph = randParagraph;
-export const randPassword = randPassword;
-export const randPastDate = randPastDate;
-export const randPermission = randPermission;
-export const randPersonTitle = randPersonTitle;
-export const randPhoneNumber = randPhoneNumber;
-export const randPhrase = randPhrase;
-export const randPort = randPort;
-export const randPost = randPost;
-export const randPriority = randPriority;
-export const randProduct = randProduct;
-export const randProductAdjective = randProductAdjective;
-export const randProductCategory = randProductCategory;
-export const randProductDescription = randProductDescription;
-export const randProductMaterial = randProductMaterial;
-export const randProductName = randProductName;
-export const randProgrammingLanguage = randProgrammingLanguage;
-export const randPronoun = randPronoun;
-export const randProtocol = randProtocol;
-export const randQuote = randQuote;
-export const randRabbit = randRabbit;
-export const rand = rand;
-export const random = random;
-export const randRecentDate = randRecentDate;
-export const randRgb = randRgb;
-export const randRole = randRole;
-export const randRoutingNumber = randRoutingNumber;
-export const randSemver = randSemver;
-export const randSentence = randSentence;
-export const randSequence = randSequence;
-export const randShape = randShape;
-export const randSkill = randSkill;
-export const randSlug = randSlug;
-export const randSnake = randSnake;
-export const randSocial = randSocial;
-export const randSoonDate = randSoonDate;
-export const randSports = randSports;
-export const randSportsTeam = randSportsTeam;
-export const randState = randState;
-export const randStateAbbr = randStateAbbr;
-export const randStatus = randStatus;
-export const randStreetAddress = randStreetAddress;
-export const randStreetName = randStreetName;
-export const randSubscriptionPlan = randSubscriptionPlan;
-export const randSuperhero = randSuperhero;
-export const randSuperheroName = randSuperheroName;
-export const randSvg = randSvg;
-export const randSwift = randSwift;
-export const randText = randText;
-export const randTimeZone = randTimeZone;
-export const randTodo = randTodo;
-export const randTransactionType = randTransactionType;
-export const randUrl = randUrl;
-export const randUser = randUser;
-export const randUserAgent = randUserAgent;
-export const randUserName = randUserName;
-export const randUuid = randUuid;
-export const randVehicle = randVehicle;
-export const randVehicleFuel = randVehicleFuel;
-export const randVehicleManufacturer = randVehicleManufacturer;
-export const randVehicleModel = randVehicleModel;
-export const randVehicleType = randVehicleType;
-export const randVerb = randVerb;
-export const randWeekday = randWeekday;
-export const randWord = randWord;
-export const randZipCode = randZipCode;
-export const randSinger = randSinger;
-export const randSong = randSong;
-export const randAddress = randAddress;
-export const randFullAddress = randFullAddress;
-export const randJSON = randJSON;
-export const randCodeSnippet = randCodeSnippet;
-export const randTextRange = randTextRange;
-export const randFlightNumber = randFlightNumber;
-export const randAirline = randAirline;
-export const randFlightDetails = randFlightDetails;
-export const randSeatNumber = randSeatNumber;
-export const randAirport = randAirport;
-export const randAirportName = randAirportName;
-export const randAirportCode = randAirportCode;
-export const randCreditCard = randCreditCard;
+export {
+    Book,
+    Post,
+    Product,
+    seed,
+    SuperheroEntity,
+    Todo,
+    User,
+    Address,
+    FlightDetails,
+    randAbbreviation,
+    randAccessory,
+    randAccount,
+    randAlpha,
+    randAlphaNumeric,
+    randAmericanFootballTeam,
+    randAmount,
+    randAnimalType,
+    randArn,
+    randAvatar,
+    randAwsRegion,
+    randAwsService,
+    randAwsRequestId,
+    randBaseballTeam,
+    randBasketballTeam,
+    randBear,
+    randBetweenDate,
+    randBic,
+    randBinary,
+    randBird,
+    randBitcoinAddress,
+    randBook,
+    randBoolean,
+    randBrand,
+    randBrowser,
+    randCardinalDirection,
+    randCat,
+    randCatchPhrase,
+    randCetacean,
+    randCity,
+    randClothingSize,
+    randColor,
+    randCompanyName,
+    randCountry,
+    randCountryCode,
+    randCounty,
+    randCow,
+    randCreditCardCVV,
+    randCreditCardBrand,
+    randCreditCardNumber,
+    randCrocodilia,
+    randCurrencyCode,
+    randCurrencyName,
+    randCurrencySymbol,
+    randDatabaseCollation,
+    randDatabaseColumn,
+    randDatabaseEngine,
+    randDatabaseType,
+    randDepartment,
+    randDirection,
+    randDirectoryPath,
+    randDog,
+    randDomainName,
+    randDomainSuffix,
+    randDrinks,
+    randEmail,
+    randEmailProvider,
+    randEmoji,
+    randEthereumAddress,
+    randFileExt,
+    randFileName,
+    randFilePath,
+    randFileType,
+    randFirstName,
+    randFish,
+    randFloat,
+    randFontFamily,
+    randFontSize,
+    randFood,
+    randFootballTeam,
+    randFrequency,
+    randFullName,
+    randFutureDate,
+    randGender,
+    randGitBranch,
+    randGitCommitEntry,
+    randGitCommitMessage,
+    randGitCommitSha,
+    randGitShortSha,
+    randHex,
+    randHexaDecimal,
+    randHorse,
+    randHsl,
+    randHttpMethod,
+    randIban,
+    randIceHockeyTeam,
+    randImg,
+    randIntegration,
+    randIp,
+    randIpv6,
+    randJobArea,
+    randJobDescriptor,
+    randJobTitle,
+    randJobType,
+    randLanguage,
+    randLastName,
+    randLatitude,
+    randLine,
+    randLines,
+    randLion,
+    randLocale,
+    randLongitude,
+    randMac,
+    randMask,
+    randMimeType,
+    randMonth,
+    randMovie,
+    randMovieCharacter,
+    randMusicGenre,
+    randNearbyGPSCoordinate,
+    randNumber,
+    randOAuthProvider,
+    randOctal,
+    randOrdinalDirection,
+    randParagraph,
+    randPassword,
+    randPastDate,
+    randPermission,
+    randPersonTitle,
+    randPhoneNumber,
+    randPhrase,
+    randPort,
+    randPost,
+    randPriority,
+    randProduct,
+    randProductAdjective,
+    randProductCategory,
+    randProductDescription,
+    randProductMaterial,
+    randProductName,
+    randProgrammingLanguage,
+    randPronoun,
+    randProtocol,
+    randQuote,
+    randRabbit,
+    rand,
+    random,
+    randRecentDate,
+    randRgb,
+    randRole,
+    randRoutingNumber,
+    randSemver,
+    randSentence,
+    randSequence,
+    randShape,
+    randSkill,
+    randSlug,
+    randSnake,
+    randSocial,
+    randSoonDate,
+    randSports,
+    randSportsTeam,
+    randState,
+    randStateAbbr,
+    randStatus,
+    randStreetAddress,
+    randStreetName,
+    randSubscriptionPlan,
+    randSuperhero,
+    randSuperheroName,
+    randSvg,
+    randSwift,
+    randText,
+    randTimeZone,
+    randTodo,
+    randTransactionType,
+    randUrl,
+    randUser,
+    randUserAgent,
+    randUserName,
+    randUuid,
+    randVehicle,
+    randVehicleFuel,
+    randVehicleManufacturer,
+    randVehicleModel,
+    randVehicleType,
+    randVerb,
+    randWeekday,
+    randWord,
+    randZipCode,
+    randSinger,
+    randSong,
+    randAddress,
+    randFullAddress,
+    randJSON,
+    randCodeSnippet,
+    randTextRange,
+    randFlightNumber,
+    randAirline,
+    randFlightDetails,
+    randSeatNumber,
+    randAirport,
+    randAirportName,
+    randAirportCode,
+    randCreditCard,
+}
 export const falso = {
     randAbbreviation,
     randAccessory,
