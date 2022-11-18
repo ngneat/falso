e1ead8dbbf2c46d4cc3811babe9ac022c79fc3bb
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-11-18 15:09:43 +0900

    refactor: 💡 more type enhancement using built-in keyword
    I made objectKeys which can infer type strictly by behaving identical
    like Object.keys() function.

--- packages/falso/src/lib/sports.ts
+++ packages/falso/src/lib/sports.ts
@@ -1,9 +1,11 @@
 import { fake, FakeOptions, getRandomInRange, randElement } from './core/core';
 import { data } from './sports.json';
 
+export type Category = keyof typeof data;
+
 export interface SportCategories extends FakeOptions {
   // This categories can be extended in the future
-  category?: 'olympic' | 'outdoor' | 'winterOlympic';
+  category?: Category;
 }
 
 const categoriesCount = Object.keys(data)?.length;
