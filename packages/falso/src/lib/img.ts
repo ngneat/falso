dbcaf144e62ec0f43dfa233d1b8f979870f087f4
Alessandro Foschi <alessandro.foschi5@gmail.com>
2023-04-04 12:53:21 +0200

    fix: 🐞 change random image service
    This changes the URL returned by randImg() from placeimg.com to
    picsum.photos.
    
    BREAKING CHANGE: 🧨 "category" option will not work anymore

--- packages/falso/src/lib/img.ts
+++ packages/falso/src/lib/img.ts
@@ -3,10 +3,8 @@ import { fake, FakeOptions } from './core/core';
 interface ImgOptions extends FakeOptions {
   width?: number;
   height?: number;
-  category?: Category;
+  grayscale?: boolean;
 }
-
-type Category = 'animals' | 'arch' | 'nature' | 'people' | 'tech';
 
 /**
  * Generate a random img.
@@ -21,18 +19,30 @@ type Category = 'animals' | 'arch' | 'nature' | 'people' | 'tech';
  *
  * randImg({ length: 10 })
  *
+ * @example
+ *
+ * randImg({ width: 300 }) // default is "height" or 500 (if not set either)
+ *
+ * @example
+ *
+ * randImg({ height: 200 }) // default is "width" or 500 (if not set either)
+ *
+ * @example
+ *
+ * randImg({ grayscale: true }) // return a grayscale image (default is false)
+ *
  */
 export function randImg<Options extends ImgOptions = never>(options?: Options) {
-  const [width, height, category] = [
-    options?.width ?? 500,
-    options?.height ?? 500,
-    options?.category ?? '',
+  const [width, height, grayscale] = [
+    options?.width ?? options?.height ?? 500,
+    options?.height ?? options?.width ?? 500,
+    options?.grayscale ?? false,
   ];
 
   return fake(
     () =>
-      `https://placeimg.com/${width}/${height}${
-        category ? `/${category}` : category
+      `https://picsum.photos/${width}/${height}${
+        grayscale ? '?grayscale' : ''
       }`,
     options
   );
